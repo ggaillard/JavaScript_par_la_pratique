6f09c0862d65c72d8cfba3d0890a1fea58eebce1
ggaillard <gaillard42@gmail.com>
2021-12-30 11:45:51 +0100

    Update test_variables01.js

--- js_project/test_variables01.js
+++ js_project/test_variables01.js
@@ -1,15 +1,15 @@
 let assert = require('assert');
 let mod = require('./variables01.js');
 
-it("Création des variables : num, txt, tmp et flt", function () {
+it("Création des variables : num, txt, bin et flt", function () {
   try
   {
     assert.equal( 42, mod.num );
     assert.equal( typeof(mod.num), "number" );
     assert.equal( "42", mod.txt );
     assert.equal( typeof(mod.txt), "string" );
-    assert.equal( true, mod.tmp );
-    assert.equal( typeof(mod.tmp), "boolean" );
+    assert.equal( true, mod.bin );
+    assert.equal( typeof(mod.bin), "boolean" );
     assert.equal( 42.42, mod.flt );
     assert.equal( typeof(mod.flt), "number" );
   }
@@ -29,5 +29,5 @@ function printMessage(channel, message)
 //Solution
 // let num=42;
 //let txt=42;
-//let tmp=true;
+//let bin=true;
 //let flt=42.42;
